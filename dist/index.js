203b1f6c4e4d69aedd7937a33d09e2329923c3af
alexrr2iggs <alexandru.bunduche@yahoo.com>
2021-09-17 14:56:29 +0200

    {input:comment}

--- dist/index.js
+++ dist/index.js
@@ -10,12 +10,12 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.statisticsTypes = exports.fixturesTypes = exports.ApiFootBallClient = void 0;
+exports.ApiFootBallClient = void 0;
 var api_football_client_1 = require("./api-football-client");
 Object.defineProperty(exports, "ApiFootBallClient", { enumerable: true, get: function () { return api_football_client_1.ApiFootBallClient; } });
 __exportStar(require("./common/types"), exports);
-var fixtures_d_1 = require("./end-points/fixtures/fixtures/fixtures.d");
-Object.defineProperty(exports, "fixturesTypes", { enumerable: true, get: function () { return fixtures_d_1.fixturesTypes; } });
-var statistics_d_1 = require("./end-points/fixtures/statistics/statistics.d");
-Object.defineProperty(exports, "statisticsTypes", { enumerable: true, get: function () { return statistics_d_1.statisticsTypes; } });
+__exportStar(require("./end-points/fixtures/fixtures/fixtures.d"), exports);
+__exportStar(require("./end-points/fixtures/statistics/statistics.d"), exports);
+__exportStar(require("./end-points/fixtures/players/types"), exports);
+__exportStar(require("./end-points/odds/odds/types"), exports);
 //# sourceMappingURL=index.js.map
